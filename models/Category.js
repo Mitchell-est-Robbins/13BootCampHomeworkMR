9210cdf824ba01a7751b2a1687a2148640bf1ca4
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-10-28 21:33:00 -0500

    updated syntax so can seed

--- models/Category.js
+++ models/Category.js
@@ -12,13 +12,12 @@ Category.init(
     primaryKey: true,
     autoIncrement: true
   },
-  category_name: {
+    category_name: {
     type: DataTypes.STRING,
     allowNull: false
   }
-
   },
-    // define columns
+    
   
   {
     sequelize,
