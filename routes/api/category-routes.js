b0c12e492b265e7f65621ed14a7c813acdc988d6
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-10-31 01:09:20 -0500

    tweaked and ran routes of category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,12 +23,12 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   router.get('/:id', async (req, res) => {
     try {
-      const categoriesData = await Driver.findByPk(req.params.id, {
+      const categoriesData = await Category.findByPk(req.params.id, {
         include: [{ model: Product }], //----------------------
       });
   
       if (!categoriesData) {
-        res.status(404).json({ message: 'No driver found with that id!' });
+        res.status(404).json({ message: 'No category found with that id!' });
         return;
       }
   
@@ -41,7 +41,7 @@ router.get('/', async (req, res) => {
 // ======POST==================================================================================
   router.post('/', async (req, res) => {
     try {
-      const categoriesData = await Location.create(req.body);
+      const categoriesData = await Category.create(req.body);
       res.status(200).json(categoriesData);
     } catch (err) {
       res.status(400).json(err);
@@ -50,7 +50,7 @@ router.get('/', async (req, res) => {
 // ====DELETE===================================================================================
   router.delete('/:id', async (req, res) => {
     try {
-      const categoriesData = await Location.destroy({
+      const categoriesData = await Category.destroy({
         where: {
           id: req.params.id
         }
@@ -67,20 +67,17 @@ router.get('/', async (req, res) => {
     }
   });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
 
 // ====PUT===================================================================================
 router.put('/:id', async (req, res) => {
   try {
-    const categoriesData = await User.update(req.body, {
+    const categoriesData = await Category.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
     if (!categoriesData[0]) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'No Category with this id!' });
       return;
     }
     res.status(200).json(categoriesData);
